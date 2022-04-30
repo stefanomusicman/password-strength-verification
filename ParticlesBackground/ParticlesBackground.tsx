ebbf1230d7a1e081dd18296ba606e94e3db62c51
Stefano Proietti <stef.pro@hotmail.com>
2022-04-29 22:06:56 -0400

    background changed, particles installed

---
+++ ParticlesBackground/ParticlesBackground.tsx
@@ -0,0 +1,10 @@
+import Particles from 'react-tsparticles';
+import ParticlesConfig from './ParticlesConfig';
+
+const ParticlesBackground = () => {
+    return (
+        <Particles params={ParticlesConfig}/>
+    )
+}
+
+export default ParticlesBackground;
