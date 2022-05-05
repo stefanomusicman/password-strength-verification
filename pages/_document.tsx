e7c790d488e6ec4abc7c8a7eb84e1111bb7279ba
Stefano Proietti <stef.pro@hotmail.com>
2022-05-05 17:16:10 -0400

    completely changed style of app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,15 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <div className='light x1'></div>
+          <div className='light x2'></div>
+          <div className='light x3'></div>
+          <div className='light x4'></div>
+          <div className='light x5'></div>
+          <div className='light x6'></div>
+          <div className='light x7'></div>
+          <div className='light x8'></div>
+          <div className='light x9'></div>
         </body>
       </Html>
     );
